ded3186064f6d19537d798817881e4bd6b0f5216
buendiaomar <buendiaomar@hotmail.com>
2022-02-21 13:59:56 -0500

    Initial Commit

---
+++ OmarController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OmarController
+//
+//  Created by Consultant on 2/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
